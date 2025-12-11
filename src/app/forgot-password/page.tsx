8673c00713c338f877532f4e309b2dc6076c64ca
jhossuarodriguez <jhossuarodriguezz@gmail.com>
2025-12-11 09:27:47 -0400

    Actualizar logos en el componente de restablecimiento de contraseña

--- src/app/forgot-password/page.tsx
+++ src/app/forgot-password/page.tsx
@@ -7,11 +7,11 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 
 export default function ForgotPassword() {
     return (
-        <div className="flex min-h-screen items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-gradient-to-br from-gray-50 to-gray-100">
-            <Image className="absolute inset-0 opacity-5 -z-10 aspect-[1024/505] object-contain w-full h-full" src='/CAID-LOGO.webp' alt='CAID Logo' width={800} height={800} />
-            <Card className="w-full max-w-sm bg-white border-gray-200 shadow-xl">
+        <div className="flex min-h-screen items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+            <Image className="absolute inset-0 opacity-5 -z-10 aspect-[1024/505] object-contain w-full h-full" src='/logo.png' alt='Logo' width={800} height={800} />
+            <Card className="w-full max-w-sm">
                 <CardHeader className="text-center flex flex-col items-center justify-items-center">
-                    <Image className='mb-4' src="/CAID-LOGO.webp" alt='Logo del CAID' width={120} height={120} />
+                    <Image className='mb-4' src="/logo.png" alt='Logo' width={120} height={120} />
                     <CardTitle className="text-3xl font-bold text-[#1A2E35]">Restablecer Contraseña</CardTitle>
                     <CardDescription className="text-gray-600 mt-2">
                         Ingresa tu correo y te enviaremos un enlace para restablecer tu contraseña
